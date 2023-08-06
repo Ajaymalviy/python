9afd85772e9392692077fb9759d8900e31dbca2a
Ajaymalviy <128165665+Ajaymalviy@users.noreply.github.com>
2023-08-06 16:53:20 +0530

    Create lambda function.py

---
+++ lambda function.py
@@ -0,0 +1,7 @@
+# your.python
+cube=lambda x:x*3
+squar=lambda x:x*x
+print(cube(5))
+print(squar(6))
+#it is just one line of fuction which do the same work which is done by our normal function
+#with this help we can hide from our lengthy code in which we had to do return pass
